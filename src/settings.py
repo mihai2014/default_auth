6686fc279eb799c9ba19ad966b868652f0b7828a
mihai2014 <mc200520052005@yahoo.com>
2020-12-25 22:15:13 +0200

    adding password reset

--- src/settings.py
+++ src/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+#fake mail deliver
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
+
